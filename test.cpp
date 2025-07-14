7e6388dc52048f062f67975f9190d4a5b13ffa5b
TuanHung <firekatana1209@gmail.com>
2025-07-14 22:48:06 +0700

    checkingChar function

--- test.cpp
+++ test.cpp
@@ -19,31 +19,51 @@ WINDOW* init(){
     return win;
 }
 
-int main(int argc, char ** argv){
-    WINDOW *curwin =  init();
-    string sentence = "please try to write like this sentence";
-    mvwprintw(curwin, 1, 1, sentence.c_str());
-    bool state = true;
+bool checkingChar(bool state, string sentence, WINDOW * win){
     int cur_chr = 0;
     int cur_x = 1;
-    while(state){
-        if(cur_chr >= sentence.size()){
+    while (state)
+    {
+        if (cur_chr >= sentence.size())
+        {
             state = false;
         }
-        int ch = wgetch(curwin);
-        if (ch == sentence[cur_chr]){
-            wattron(curwin,COLOR_PAIR(1));
-            mvwprintw(curwin, 2, cur_x, "%c", sentence[cur_chr]); 
-            wattroff(curwin, COLOR_PAIR(1));
+
+        int ch = wgetch(win);
+        if (ch == sentence[cur_chr])
+        {
+            wattron(win, COLOR_PAIR(1));
+            mvwprintw(win, 2, cur_x, "%c", sentence[cur_chr]);
+            wattroff(win, COLOR_PAIR(1));
         }
-        else{
-            wattron(curwin, COLOR_PAIR(2));
-            mvwprintw(curwin, 2, cur_x, "%c", sentence[cur_chr]);
-            wattroff(curwin, COLOR_PAIR(2));
+        else
+        {
+            wattron(win, COLOR_PAIR(2));
+            mvwprintw(win, 2, cur_x, "%c", sentence[cur_chr]);
+            wattroff(win, COLOR_PAIR(2));
         }
-        wrefresh(curwin);
+        wrefresh(win);
         cur_chr++;
         cur_x++;
+    }
+    return false;
+}
+
+int main(int argc, char ** argv){
+    WINDOW *curwin =  init();
+    bool curstate = true;
+
+    while (curstate)
+    {
+        // try to create a function thaty take random phraise to from a text file (would be great to learn wtf is api)
+        string sentence = "please try to write like this sentence";
+        mvwprintw(curwin, 1, 1, sentence.c_str());
+
+        // create a funtion that check type per min (haredest)
+
+        // create a funtuntion to wrap this check if correct word
+        curstate = checkingChar(curstate, sentence, curwin);
+        // create a end loop that tell should player continue or stop
     }
     getch();
     
